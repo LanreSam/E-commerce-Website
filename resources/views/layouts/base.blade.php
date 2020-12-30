58d6d993f1c3586ee388b97c4e1b70040e16aabe
LanreSam <ezechelanre@gmail.com>
2020-12-30 23:22:25 +0100

    authentication

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -70,21 +70,22 @@
                                     @auth
                                         @if(Auth::user()->utype === 'ADM')
                                             <li class="menu-item menu-item-has-children parent" >
-                                                <a title="Dollar (USD)" href="#">Dollar (USD)<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                                <a title="My Account" href="#">My Account ({{ Auth::user()->name }})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
                                                 <ul class="submenu curency" >
                                                     <li class="menu-item" >
-                                                        <a title="Pound (GBP)" href="#">Pound (GBP)</a>
-                                                    </li>
-                                                    <li class="menu-item" >
-                                                        <a title="Euro (EUR)" href="#">Euro (EUR)</a>
-                                                    </li>
-                                                    <li class="menu-item" >
-                                                        <a title="Dollar (USD)" href="#">Dollar (USD)</a>
+                                                        <a title="Dashboard" href="#">Dashboard</a>
                                                     </li>
                                                 </ul>
                                             </li>
                                         @else
-                                            //CUSTOMER
+											<li class="menu-item menu-item-has-children parent" >
+												<a title="My Account" href="#">My Account ({{ Auth::user()->name }})<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+												<ul class="submenu curency" >
+													<li class="menu-item" >
+														<a title="Dashboard" href="#">Dashboard</a>
+													</li>
+												</ul>
+											</li>
                                         @endif
                                     @else
                                         <li class="menu-item" ><a title="Register or Login" href="{{ route('login') }}">Login</a></li>
