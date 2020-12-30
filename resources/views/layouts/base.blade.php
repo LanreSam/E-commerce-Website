de346fa240a98d62a2c4b3d0acb14ccffcb3ed7e
LanreSam <ezechelanre@gmail.com>
2020-12-30 18:49:26 +0100

    working on the app views

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -43,8 +43,6 @@
 						</div>
 						<div class="topbar-menu right-menu">
 							<ul>
-								<li class="menu-item" ><a title="Register or Login" href="login.html">Login</a></li>
-								<li class="menu-item" ><a title="Register or Login" href="register.html">Register</a></li>
 								<li class="menu-item lang-menu menu-item-has-children parent">
 									<a title="English" href="#"><span class="img label-before"><img src="assets/images/lang-en.png" alt="lang-en"></span>English<i class="fa fa-angle-down" aria-hidden="true"></i></a>
 									<ul class="submenu lang" >
@@ -67,7 +65,32 @@
 											<a title="Dollar (USD)" href="#">Dollar (USD)</a>
 										</li>
 									</ul>
-								</li>
+                                </li>
+                                @if(Route::has('login'))
+                                    @auth
+                                        @if(Auth::user()->utype === 'ADM')
+                                            <li class="menu-item menu-item-has-children parent" >
+                                                <a title="Dollar (USD)" href="#">Dollar (USD)<i class="fa fa-angle-down" aria-hidden="true"></i></a>
+                                                <ul class="submenu curency" >
+                                                    <li class="menu-item" >
+                                                        <a title="Pound (GBP)" href="#">Pound (GBP)</a>
+                                                    </li>
+                                                    <li class="menu-item" >
+                                                        <a title="Euro (EUR)" href="#">Euro (EUR)</a>
+                                                    </li>
+                                                    <li class="menu-item" >
+                                                        <a title="Dollar (USD)" href="#">Dollar (USD)</a>
+                                                    </li>
+                                                </ul>
+                                            </li>
+                                        @else
+                                            //CUSTOMER
+                                        @endif
+                                    @else
+                                        <li class="menu-item" ><a title="Register or Login" href="{{ route('login') }}">Login</a></li>
+                                        <li class="menu-item" ><a title="Register or Login" href="{{ route('register') }}">Register</a></li>
+                                    @endif
+                                @endif
 							</ul>
 						</div>
 					</div>
